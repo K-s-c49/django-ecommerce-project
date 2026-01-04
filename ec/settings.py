82cdcd3ae0bac64ee1c4bd074f87f713be36e389
khushal singh chundawat <khushalsinghchundawat144@gmail.com>
2026-01-04 17:25:59 +0530

    Change database name to 'khushalsingh$default'

--- ec/settings.py
+++ ec/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'ec.wsgi. application'
 DATABASES = {
     'default':  {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'khushalsingh$myecommerce_db',
+        'NAME': 'khushalsingh$default',
         'USER': 'khushalsingh',
         'PASSWORD': 'khushal1132',
         'HOST':  'khushalsingh.mysql.pythonanywhere-services.com',
